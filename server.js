26444d7d68779326dc6985a501dbe142b42697c1
Penny <radiogaga66@gmail.com>
2019-03-20 16:32:42 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+//REQUIREMENTS
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("Listening on port %s", port));
